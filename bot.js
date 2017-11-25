67c743e20554b56cb82905983bf0d25b481ec1d2
hffladmin <tetrarc@live.com>
2017-11-25 10:19:05 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -160,6 +160,23 @@ function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
+function draftCountdown {
+  // Set the date we're counting down to
+  var countDownDate = new Date("Nov 29, 2017 18:00:00").getTime();
+  
+  //var currentDate = new.Date().getTime();
+
+  // Time calculations for days, hours, minutes and seconds
+  //var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  //var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  //var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  //var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  
+  // Find the distance between now an the count down date
+  //var remainingTime = countDownDate - now;
+  //return remainingTime;
+}
+
 
 
 
