87bbc0e862b047dd38892b80502c423f8e3bf582
hffladmin <tetrarc@live.com>
2017-11-25 10:50:46 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,6 @@
 var HTTPS = require('https');
 var cool = require('cool-ascii-faces');
+const request = require("tinyreq");
 
 var botID = process.env.BOT_ID;
 
@@ -78,7 +79,7 @@ function respond() {
   }
   else if(request.text && botRegexWhenIsDraft.test(request.text)) {
     this.res.writeHead(200);
-    postMessage("Next Draft: Nov 29 @ 6PM PST \n Results: https://imgur.com/a/w8CGb \n Remaining Time: https://hffldraftcountdown.herokuapp.com/");
+    postMessage("Next Draft: Nov 29 @ 6PM PST \n Results: https://imgur.com/a/w8CGb");
     this.res.end();
   }
   else if(request.text && botRegexTradeBlock.test(request.text)) {
