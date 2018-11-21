f776ab99cea184c85d1c03e6bca3a5ca7764dac7
HFFL Admin <tetrarc@live.com>
2018-11-21 15:02:05 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -71,7 +71,7 @@ function respond() {
   }
   else if(request.text && botRegexTrophyCase.test(request.text)) {
     this.res.writeHead(200);
-    postMessage("Trophy Case (Madden 19) \n Year 1:");
+    postMessage("Trophy Case (Madden 19) \n Year 1: Biz (Jaguars)");
     this.res.end();
   }
   else if(request.text && botRegexWhenIsDraft.test(request.text)) {
